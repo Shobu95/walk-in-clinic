ea033c63962887163fbda22f611632ff687d0a73
Shoaib <shoaib.ahmed@ivacy.com>
2023-07-02 22:09:03 +0500

    Created BookAppointment Screen with Selected clinic name, datepicker, slot picker, reason text area and booking button.

---
+++ app/src/main/java/com/shobu/walk_in_clinic/ui/components/BorderedTextArea.kt
@@ -0,0 +1,103 @@
+package com.shobu.walk_in_clinic.ui.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.shobu.walk_in_clinic.R
+
+@Preview
+@Composable
+fun BorderedTextAreaPrev() {
+    Box(
+        modifier = Modifier
+            .padding(10.dp)
+    ) {
+//        BorderedTextField()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BorderedTextArea(
+    textFieldValue: MutableState<String>,
+    @StringRes hint: Int,
+    modifier: Modifier = Modifier,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    onTextChange: (value: String) -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .height(100.dp)
+            .border(
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = Color.Black
+                ),
+                shape = RoundedCornerShape(8.dp)
+            ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        TextField(
+            modifier = modifier
+                .fillMaxSize(),
+            value = textFieldValue.value,
+            onValueChange = {
+                if (!it.contains("\n")) {
+                    textFieldValue.value = it
+                    onTextChange(it)
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+            maxLines = 5,
+            textStyle = TextStyle(
+                fontSize = 13.sp,
+                fontFamily = FontFamily(
+                    Font(R.font.roboto)
+                )
+            ),
+            placeholder = {
+                Text(
+                    text = stringResource(id = hint),
+                    style = TextStyle(
+                        fontSize = 13.sp,
+                        textAlign = TextAlign.Center,
+                        fontFamily = FontFamily(
+                            Font(R.font.roboto)
+                        )
+                    ),
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+            ),
+        )
+    }
+}
