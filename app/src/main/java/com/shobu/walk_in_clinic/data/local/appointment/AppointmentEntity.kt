569bca21d6c5222bb986ec7475538e81485dcf8e
Shoaib <shoaib.ahmed@ivacy.com>
2023-07-07 01:57:34 +0500

    Fixed design of RateAndReviewDialog.

---
+++ app/src/main/java/com/shobu/walk_in_clinic/data/local/appointment/AppointmentEntity.kt
@@ -0,0 +1,14 @@
+package com.shobu.walk_in_clinic.data.local.appointment
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "appointment_table")
+data class AppointmentEntity(
+    @PrimaryKey var id: Int? = null,
+    var locationName: String,
+    var date: String,
+    var slot: String,
+    var reason: String,
+    var status: String,
+)
