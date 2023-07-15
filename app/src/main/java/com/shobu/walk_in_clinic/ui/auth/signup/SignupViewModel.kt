c58eaef98a6613db0cec2e9fac4c9ce2bd186fa4
Shoaib <shoaib.ahmed@ivacy.com>
2023-07-15 19:26:44 +0500

    Added duplicate email validation on signup.

--- app/src/main/java/com/shobu/walk_in_clinic/ui/auth/signup/SignupViewModel.kt
+++ app/src/main/java/com/shobu/walk_in_clinic/ui/auth/signup/SignupViewModel.kt
@@ -4,9 +4,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.shobu.walk_in_clinic.domain.models.User
 import com.shobu.walk_in_clinic.domain.use_cases.authentication.CreateUserUseCase
 import com.shobu.walk_in_clinic.domain.use_cases.authentication.SaveUserSessionUseCase
+import com.shobu.walk_in_clinic.domain.use_cases.authentication.SignupUseCase
+import com.shobu.walk_in_clinic.domain.use_cases.authentication.SignupUseCaseResponse
 import com.shobu.walk_in_clinic.domain.use_cases.validations.ValidateUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -14,6 +17,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,7 +26,8 @@ class SignupViewModel
 @Inject constructor(
     private val createUserUseCase: CreateUserUseCase,
     private val saveUserSession: SaveUserSessionUseCase,
-    private val validateUserUseCase: ValidateUserUseCase
+    private val validateUserUseCase: ValidateUserUseCase,
+    private val signupUseCase: SignupUseCase
 ) : ViewModel() {
 
     var state by mutableStateOf(SignupState())
@@ -30,6 +35,35 @@ class SignupViewModel
 
     private var _signupFailState = MutableSharedFlow<SignupFailedState>()
     val signupFailState: SharedFlow<SignupFailedState> = _signupFailState.asSharedFlow()
+
+    private lateinit var newUser: User
+
+    init {
+        viewModelScope.launch {
+            signupUseCase.signupState.collectLatest { event ->
+                when (event) {
+                    SignupUseCaseResponse.OnEmailExists -> {
+                        _signupFailState.emit(
+                            SignupFailedState(
+                                isFailed = true,
+                                message = "You're already signed up with this email"
+                            )
+                        )
+                    }
+
+                    SignupUseCaseResponse.OnEmailNotExists -> {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            createUserUseCase(user = newUser)
+                            state = state.copy(
+                                signupSuccess = true
+                            )
+                            saveUserSession(newUser)
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
     fun onEvent(event: SignupEvents) {
@@ -42,7 +76,7 @@ class SignupViewModel
             is SignupEvents.OnSignupClicked -> {
                 CoroutineScope(Dispatchers.IO).launch {
 
-                    val newUser = User(
+                    newUser = User(
                         id = null,
                         fullName = state.fullName,
                         phoneNumber = state.phoneNumber,
@@ -55,11 +89,7 @@ class SignupViewModel
                     )
 
                     if (validateUserUseCase(state).first) {
-                        createUserUseCase(user = newUser)
-                        state = state.copy(
-                            signupSuccess = true
-                        )
-                        saveUserSession(newUser)
+                        signupUseCase.invoke(state.email)
                     } else
                         _signupFailState.emit(
                             SignupFailedState(
